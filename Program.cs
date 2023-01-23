7599fe5ab8a72bc7528aefcedec8def33bddf1b4
T N Thanoof Naina <thanoof.tn@experionglobal.local>
2023-01-23 14:25:23 +0530

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Enter the number of entries: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+string[] passengerName = new string[n];
+string[] passengerDestination = new string[n];
+string[] travelDetails = new string[2 * n];
+
+for (int i = 0; i < n; i++)
+{
+    Console.Write("Enter the passenger name: ");
+    passengerName[i] = Console.ReadLine();
+
+    Console.Write($"Enter the destination for {passengerName[i]}: ");
+    passengerDestination[i] = Console.ReadLine();
+}
+
+travelDetails = passengerName.Concat(passengerDestination).ToArray();
+
+Console.WriteLine(string.Join(" , ", travelDetails));
